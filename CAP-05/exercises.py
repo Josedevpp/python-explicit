c6f42e43ebd70f0803742b5f100a9693e26d2e9b
Josedevpp <codesimplifyy@gmail.com>
2025-03-30 17:25:59 -0600

    Add exercises and /img.

---
+++ CAP-05/exercises.py
@@ -0,0 +1,20 @@
+#1.Crea una lista con los numeros del [1...9] é imprime el resultado
+
+#2.Accede é imprime al numero 8 de la anterior lista, en base a su indice
+
+#3.Agrega un numero 10 al final de la lista anterior
+
+#4.inserta el numero 15, en la posición 2 de la lista
+
+#5.Elimina el numero 15 de la lista
+
+#6.Usa la funcion pop, para eliminar el ultimo elemento
+
+#7.Invierte la lista
+
+#8.Ordena la siguiente lista [2, 5, 6, 9, 1]
+
+#9.Crea una nueva variable y concatena las dos listas anteriores
+
+#10.Crea una sublista de lista_numeros de la posicion 1..3, sin tomar en cuenta el indice 3
+
