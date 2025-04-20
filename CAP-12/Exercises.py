c2ee9176f129aeba7afe1e1acecf968c9ff0355c
Josedevpp <codesimplifyy@gmail.com>
2025-04-20 13:26:06 -0600

    Modified exercises.py

--- CAP-12/Exercises.py
+++ CAP-12/Exercises.py
@@ -1,8 +1,8 @@
 #1.Crea una classe llamada "Animal" y que tenga la propiedad de "especie" y un metodo "make_sound" que imprima un sonido generico.
-
+    
 #2.Modifica la classe "Animal" para que reciba la especie al crear el objeto y almacenala en una propiedad publica, añade el metodo "make_sound" que imprima el sonido dependiendo de la especie.
 
-#3.Crea una classe llamada "Auto" con las propiedades publicas "marca", "modelo" y ademas debe tener una propiedad privada "__speed" que inicialmente sea 0.
+#3.Crea una classe llamada "Auto" con las propiedades publicas "marca", "modelo" y ademas debe tener una propiedad privada "__speed" que inicialmente sea 0. 
 
 #4.Añade a la classe "Auto" un meto do "accelerate" que aumente la velocidad en 10 y un metodo "brake" que disminuya la velocidad en 10, asegurandote de que la velocidad no sea negativa.
 
@@ -12,10 +12,8 @@
 
 #7.Crea una classe "BankAccount" que tenga propiedades como "titular", "saldo" y un metodo depositar y retirar dinero, asegurandote de que el saldo no sea negativo.
 
-#8.Crea una classe "point" que representa un punto en un plano 2D, con propiedades "x" y "y", y un metodo que calcule la distancia entre dos puntos.
+#8.Crea una classe "Point" que representa un punto en un plano 2D, con propiedades "x" y "y", y un metodo que calcule la distancia entre dos puntos.
 
-
-#9.Crea una classe "Employee" que tenga propiedades como "nombre", "hourly_wage" y "horas trabajadas", añade un metodo que calcule el apgo total basado en las horas trabajadas y el saladio por hora.
-
+#9.Crea una classe "Employee" que tenga propiedades como "nombre", "hourly_wage" y "horas trabajadas", añade un metodo que calcule el pago total basado en las horas trabajadas y el saladio por hora.
 
 #10.Crea una classe "Store" que tenga una propiedad "inventory" (una lista de productos) y un metodo "add_product" que añada un producto al inventario y otro que muestre todos los productos en el inventario.
